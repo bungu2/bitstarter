6c45ba5ccb87f1482ecdc118dfbc73770ff81860
bungu2 <bungu2@yahoo.com>
2013-07-08 18:28:25 +0200

    Updated web.js for syntax

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 var buffer = new Buffer(fs.readFileSync('index.html'), "utf-8");
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf-8");
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
